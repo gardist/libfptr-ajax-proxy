e9a990b63ae973620f4d084427ae2551efd354fb
gardist <50888847+gardist@users.noreply.github.com>
2019-05-22 19:08:54 +0300

    Update compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -3,4 +3,4 @@
 LIBFPTRPATH="/home/pi/atol/10.4.6/ajaxproxy/java/*"
 PROXYPATH="/home/pi/atol/10.4.6/ajaxproxy/src/proxydrv/proxydrv.java"
 
-/usr/bin/javac -cp $LIBFPTRPATH $PROXYP
+/usr/bin/javac -cp $LIBFPTRPATH $PROXYPATH
