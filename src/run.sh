e2c3649842248f407ec14f07456b30330edfd9d3
gardist <50888847+gardist@users.noreply.github.com>
2019-05-23 14:34:48 +0300

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LIBFPTRPATH="/home/pi/atol/10.4.6/ajaxproxy/java/*"
+LIBFPTRPATH="/home/pi/atol/10.4.6/jsonproxy/java/*"
 DATA="{\"type\":\"getShiftStatus\"}"
 
 /usr/bin/java -classpath .:$LIBFPTRPATH proxydrv.proxydrv $DATA
